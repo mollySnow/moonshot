b7475d54402b5f17654a2e86443be6e986bed594
Molly <molly.k.bjork@gmail.com>
2020-11-08 17:31:52 +0100

    init godot project

---
+++ src/nodes/moon/Gun.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+onready var end_of_barrel = $BarrelEnd
+onready var bullet_packed = preload("res://nodes/bullet/bullet.tscn")
+
+export var force = 2;
+
+signal gun_shot;
+
+
+# Called when the node enters the scene tree for the first time.
+var mouse_position = Vector2.ZERO
+
+func shoot():
+	var direction = (end_of_barrel.global_position - global_position).normalized()
+	var boom = 500
+	var bullet = bullet_packed.instance()
+	bullet.position = end_of_barrel.global_position
+	bullet.linear_velocity = direction * boom
+	bullet.set_name("why")
+	get_parent().get_parent().bullet_layer.add_child(bullet)
+
+
+func _process(_delta):
+	if Input.is_action_pressed("lmb"):
+		var rot = (get_global_mouse_position() - .get_parent().global_position).normalized().angle()
+		rotation = rot
+	if Input.is_action_just_pressed("ui_accept"):
+		shoot()
+		emit_signal("gun_shot", -(end_of_barrel.global_position - global_position), force)
---
+++ src/nodes/moon/moon.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export (NodePath) var orbit_node_path
+export (NodePath) var bullet_layer_path
+onready var orbit = get_node(orbit_node_path)
+onready var bullet_layer = get_node(bullet_layer_path)
+onready var body = $RigidBody2D
+onready var gravity_orbit = $GravityFieldOrbit
+
+func _ready():
+	print("moon.gd")
+	print(bullet_layer)
+
+func _process(_delta):
+	gravity_orbit.rotation = body.position.angle()
+
+func _on_Gun_gun_shot(v, force):
+	body.apply_central_impulse(v * force)
