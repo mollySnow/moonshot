f4306c83a332f43c3181718d43d7ee8d42f499dc
Molly <molly.k.bjork@gmail.com>
2020-11-09 21:50:44 +0100

    vscode git integration is not for people used to actual git

---
+++ src/events/signals.gd
@@ -0,0 +1,9 @@
+extends Node
+signal gun_shot
+var signal_gun_shot = "gun_shot"
+
+func gun_shot_emit(v,f):
+    emit_signal("gun_shot", v, f)
+
+func gun_shot_connect(obj, f_name):
+    connect("gun_shot", obj, f_name)
