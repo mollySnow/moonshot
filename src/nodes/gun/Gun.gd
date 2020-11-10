8de3324a965709bc996c315d96882fac33159362
molly bjoerk <molly.k.bjork@gmail.com>
2020-11-10 11:19:57 +0100

    fixed bullet spawnpoint, moon rotates with torque

--- src/nodes/gun/Gun.gd
+++ src/nodes/gun/Gun.gd
@@ -5,9 +5,6 @@ onready var bullet_packed = preload("res://nodes/bullet/bullet.tscn")
 
 export var force = 2;
 
-signal gun_shot;
-
-
 # Called when the node enters the scene tree for the first time.
 var mouse_position = Vector2.ZERO
 
@@ -15,16 +12,9 @@ func shoot():
 	var direction = (end_of_barrel.global_position - global_position).normalized()
 	var boom = 500
 	var bullet = bullet_packed.instance()
-	bullet.position = end_of_barrel.global_position
-	bullet.linear_velocity = direction * boom
 	bullet.set_name("why")
 	get_parent().get_parent().bullet_layer.add_child(bullet)
-
-
-func _process(_delta):
-	if Input.is_action_pressed("lmb"):
-		var rot = (get_global_mouse_position() - .get_parent().global_position).normalized().angle()
-		rotation = rot
-	if Input.is_action_just_pressed("ui_accept"):
-		shoot()
-		Signals.gun_shot_emit(-(end_of_barrel.global_position - global_position), force)
+	bullet.global_position = end_of_barrel.global_position
+	bullet.linear_velocity = direction * boom
+	bullet.rotation = direction.angle()
+	Signals.gun_shot_emit(-(end_of_barrel.global_position - global_position), force)
--- src/nodes/moon/moon.gd
+++ src/nodes/moon/moon.gd
@@ -6,12 +6,22 @@ onready var orbit = get_node(orbit_node_path)
 onready var bullet_layer = get_node(bullet_layer_path)
 onready var body = $RigidBody2D
 onready var gravity_orbit = $GravityFieldOrbit
+onready var gun = $RigidBody2D/Gun
 
 func _ready():
 	Signals.gun_shot_connect(self, "_on_Gun_gun_shot")
 
+
 func _process(_delta):
 	gravity_orbit.rotation = body.position.angle()
+
+	if Input.is_action_pressed("lmb"):
+		var rot = body.get_local_mouse_position().normalized().angle()
+		print("mouse angle: ", rot, " body rot: ", body.global_rotation)
+		body.apply_torque_impulse(rot * 100)
+	if Input.is_action_just_pressed("ui_accept"):
+		gun.shoot()
+
 
 func _on_Gun_gun_shot(v, force):
 	body.apply_central_impulse(v * force)
